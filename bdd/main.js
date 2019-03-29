e3aba2621b10d0cd2255c8a3400657edfae01a4b
Ana María <anamariasosam@gmail.com>
2019-03-29 12:15:28 -0500

    Update main.js

--- bdd/main.js
+++ bdd/main.js
@@ -1,4 +1,4 @@
-const average = (numbers) => numbers.reduce((a,b) => a + b, 0) / numbers.length
+const average = numbers => numbers.reduce((a,b) => a + b, 0) / numbers.length
 
 module.exports = {
 	average,
