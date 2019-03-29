9e85791be1c44215abc88b13b284c164a3ecaa55
Ana María <anamariasosam@gmail.com>
2019-03-29 12:14:45 -0500

    Update test.js

--- bdd/test.js
+++ bdd/test.js
@@ -3,17 +3,9 @@ const sample = require('./main')
 
 const numbers = [ 1, 2, 3 ]
 
-const functionExists = () => {
-	assert(typeof sample.average === 'function', 'La función no existe')
-}
-
-const functionReturnsANumber = () => {
-	assert(typeof sample.average(numbers) === 'number', 'La función no retorna un numero')
-}
-
-const functionReturnsAverage = () => {
-	assert(sample.average(numbers) === 2, 'La función no retorna el average')
-}
+const functionExists = () => assert(typeof sample.average === 'function', 'La función no existe')
+const functionReturnsANumber = () => assert(typeof sample.average(numbers) === 'number', 'La función no retorna un numero')
+const functionReturnsAverage = () => assert(sample.average(numbers) === 2, 'La función no retorna el average')
 
 functionExists()
 functionReturnsANumber()
